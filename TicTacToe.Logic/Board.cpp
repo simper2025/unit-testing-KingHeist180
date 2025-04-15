c0579df50fde385ca5a322c6bb4b569157e2fe7c
Kade Jorgensen1 <kade.jorgensen1@students.snow.edu>
2025-04-15 09:19:37 -0600

    Fixed Tie condition again

--- TicTacToe.Logic/Board.cpp
+++ TicTacToe.Logic/Board.cpp
@@ -29,15 +29,8 @@ bool Board::SetSquare(int row, int col, char player)
 
 char Board::GameStatus()
 {
-	int tie = 0;
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			if (grid[i][j] != ' ') {
-				tie++;
-			}
-		}
-	}
-	if (tie == 9)
+	//Tie condition
+	if (grid[0][0] != ' ' && grid[0][1] != ' ' && grid[0][2] != ' ' && grid[1][0] != ' ' && grid[1][1] != ' ' && grid[1][2] != ' ' && grid[2][0] != ' ' && grid[2][1] != ' ' && grid[2][2] != ' ')
 		return 'T';
 
 	//Win Conditions for diagonals
